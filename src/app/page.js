cadbab4a73d899b50de5369775c8b37385ae2f4b
Nəzrin Quliyeva <140939317+NazrenG@users.noreply.github.com>
2025-03-12 02:47:59 +0400

    next.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,102 +2,122 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div className="w-full h-screen flex flex-col items-center">
+      {/* Navbar */}
+      <nav className="bg-white fixed w-full z-20 top-0 start-0 ">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/eos-(eos).png"
+              alt="LaslesVPN Logo"
+              width={24}
+              height={24}
             />
-            Deploy now
+            <span className="font-rubik text-xl">
+              Lasles<span className="font-bold">VPN</span>
+            </span>
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <div className="flex md:order-3 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button type="button" className="text-black font-rubik font-normal">
+              Sign in
+            </button>
+            <button
+              type="button"
+              className="text-red-600 rounded-full border-red-600 border-2 px-4 py-2 font-rubik font-normal ml-4"
+            >
+              Sign up
+            </button>
+          </div>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
           >
-            Read our docs
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-white">
+              <li>
+                <a
+                  href="#"
+                  className="font-rubik font-normal self-center whitespace-nowrap block py-2 px-3"
+                  aria-current="page"
+                >
+                  features
+                </a>
+              </li>
+              <li>
+                <a href="#" className="font-rubik font-normal block py-2 px-3">
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a href="#" className="font-rubik font-normal block py-2 px-3">
+                  Testimonial
+                </a>
+              </li>
+              <li>
+                <a href="#" className="font-rubik font-normal block py-2 px-3">
+                  Help
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="w-full max-w-screen-xl mx-auto px-6 flex justify-between items-center pt-24">
+        <div className="max-w-2xl p-6 bg-white">
+          <h5 className="mb-4 font-rubik font-normal text-6xl text-gray-900 leading-tight">
+            Want anything to be easy with{" "}
+            <span className="font-bold">LaslesVPN</span>
+          </h5>
+          <p className="mb-6 text-[16px] leading-6 text-gray-700">
+            Lorem ipsum dolor sit amet consectetur. Quis tortor gravida nibh
+            arcu id purus ullamcorper. Vel vel erat semper augue.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center px-6 py-3 w-48 justify-center text-lg font-medium text-white bg-red-700 rounded-full hover:bg-red-800"
+          >
+            Get Started
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <div className="w-auto h-auto max-w-full">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/image_first.png"
+            width={600}
+            height={356}
+            className="object-cover"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+      </div>
+
+      <div className="w-full max-w-screen-xl mx-auto px-6 flex justify-between items-center pt-24">
+        <div className="w-auto h-auto max-w-full">
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/image_second.png"
+            width={600}
+            height={356}
+            className="object-cover"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+        <div className="max-w-2xl p-6 bg-white">
+          <h5 className="mb-4 font-rubik font-normal text-6xl text-gray-900 leading-tight">
+            Want anything to be easy with{" "}
+            <span className="font-bold">LaslesVPN</span>
+          </h5>
+          <p className="mb-6 text-[16px] leading-6 text-gray-700">
+            Lorem ipsum dolor sit amet consectetur. Quis tortor gravida nibh
+            arcu id purus ullamcorper. Vel vel erat semper augue.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center px-6 py-3 w-48 justify-center text-lg font-medium text-white bg-red-700 rounded-full hover:bg-red-800"
+          >
+            Get Started
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
